1abe90b4b0c41ba775fc658b37444871a8bcde32
Juan Lopes <me@juanlopes.net>
2013-02-15 19:57:15 -0200

    simplifying compiler.py

--- compiler.py
+++ compiler.py
@@ -3,17 +3,21 @@ from __future__ import print_function
 from parser import Parser
 from codegen import CodeGenerator
 
-class DojoCompiled(object):
+def dojo_compile(source, filename='<string>'):
+    ast = Parser(source).program()
+
+    #Here should come the compiler optimizations. Should.
+
+    code = CodeGenerator(filename=filename)
+    code.emit(ast)        
+    return DojoCallable(code.assemble())
+
+class DojoCallable(object):
     def __init__(self, code):
         self.code = code
         
     def __call__(self, globals = None, locals = None):
         return eval(self.code, globals, locals)
-
-def dojo_compile(source, filename='<string>'):
-    code = CodeGenerator(filename=filename)
-    code.emit(Parser(source).program())        
-    return DojoCompiled(code.assemble())
     
 if __name__ == '__main__':
     import sys, __builtin__
@@ -25,7 +29,5 @@ if __name__ == '__main__':
         print(*messages)
     
     with open(sys.argv[1]) as f:
-        data = f.read()
-        compiled = dojo_compile(data, filename=sys.argv[1])
-        compiled()
+        dojo_compile(f.read(), filename=sys.argv[1])()
 
